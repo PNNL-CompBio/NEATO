bb6d589b5d69386b1489d9ee8c522f400b4e66c8
Lewis <lewi052@we41764.pnl.gov>
2022-04-15 16:25:01 -0700

    added expression table to app.r, added clusterProfiler exploration

--- Practice_Shiny_app/app.R
+++ Practice_Shiny_app/app.R
@@ -12,7 +12,19 @@ ui <- fluidPage(selectInput(inputId = "regulation",
                 selectInput(inputId = "Comparision",
                             label = "Choose COVID-19 patient status comparision",
                             choices = list("Healthy vs Severe", "Mild vs Healthy", "Mild vs Severe")),
-                plotOutput("network"))
+                sliderInput(inputId = "included_prots",
+                            label = "Choose the number of proteins to include in the plot",
+                            min = 10,
+                            max = 500,
+                            value = 150),
+                sliderInput(inputId = "score_thresh",
+                            label = "Choose a confidence threshold for protein interactions",
+                            min = 100,
+                            max = 800,
+                            value = 400),
+                plotOutput("network"),
+                dataTableOutput("enrich")
+                )
 
 
 server <- function(input, output) {
@@ -35,23 +47,42 @@ server <- function(input, output) {
       colnames(proStat)[which(names(proStat) == "Flag_A_Mild_vs_Severe")] <- "Flag"
     }
     
+    string_db <- STRINGdb$new(version="11", species=9606, score_threshold=input$score_thresh, input_directory="/Users/lewi052/MAP/STRINGdb_exploration/STRINGdb_cache")
+    
     #filtering for the appropriate regulation and significant p-value
     
     filtered_stat <- filter(proStat, Flag == reg) %>% filter(P_value < 0.05)
     
     #ordering by logFC
-    filtered_stat[order(filtered_stat$`Fold_change`), ]
+    filtered_stat <- filtered_stat[order(filtered_stat$`Fold_change`), ]
     
     mapped_stat <- string_db$map(filtered_stat, "Protein_Identifier", removeUnmappedRows = TRUE )
-    if (nrow(mapped_stat) >= 150) {
-    hits <- mapped_stat$STRING_id[1:150]
-    string_db$plot_network(hits)
-    } else {
-    hits <- mapped_stat$STRING_id[1:150]
-    string_db$plot_network(hits)
-    }
     
+    hits <- mapped_stat$STRING_id[1:input$included_prots]
+    wd <- getwd()
+    file <- paste(wd, "hits.RDS", sep = "/")
+    saveRDS(hits, file = file)
+    
+    string_db$plot_network(hits)
+    
+  })
+  output$enrich <- renderDataTable({
+    tmp <- input$regulation
+    tmp <- input$Comparision
+    tmp <- input$included_prots
+    
+    string_db <- STRINGdb$new(version="11", species=9606, score_threshold=input$score_thresh, input_directory="/Users/lewi052/MAP/STRINGdb_exploration/STRINGdb_cache")
+    wd <- getwd()
+    file <- paste(wd, "hits.RDS", sep = "/")
+    hits <- readRDS(file)
+    enrichment <- string_db$get_enrichment(hits)
+    enrichment
   })
 }
 
 shinyApp(ui = ui, server = server)
+
+
+
+
+
