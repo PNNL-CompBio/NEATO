ff7f6471f32c0e06da7cd5edbb41a1896ae2bca7
Loglew12 <loganlewis51@gmail.com>
2022-05-19 13:20:30 -0700

    added dashboard with pcsf tab, added cluster selection

--- Prototype_app/app.R
+++ Prototype_app/app.R
@@ -6,10 +6,20 @@ library(visNetwork)
 library(igraph)
 library(stringr)
 library(shinycssloaders)
+library(shinydashboard)
 
 # defines elements of page
-ui <- fluidPage(
-  useShinyjs(),
+ui <- dashboardPage(
+  dashboardHeader(title = "MAP Enrichment"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("STRING", tabName = "STRING"),
+      menuItem("PCSF", tabName = "PCSF")
+    )
+  ),
+  dashboardBody(
+  tabItems(
+    tabItem(tabName = "STRING",
   #separates page into sidebar and main page
   navbarPage("MAP Enrichment Analysis",
              tabPanel("Data Upload",
@@ -35,21 +45,16 @@ ui <- fluidPage(
               dataTableOutput("previewTable")
             )),
     tabPanel("Network and Enrichment", 
-             sidebarPanel(sliderInput(inputId = "included_prots",
+             sidebarPanel(textInput(inputId = "included_prots",
                                       label = "Choose the number of proteins to include in the plot",
-                                      min = 10,
-                                      max = 500,
                                       value = 150),
-                          sliderInput(inputId = "score_thresh",
-                                      label = "Choose a confidence threshold for protein interactions",
-                                      min = 100,
-                                      max = 800,
+                          textInput(inputId = "score_thresh",
+                                      label = "Choose a score threshold for protein interactions",
                                       value = 400),
                           actionButton(inputId = "submit",
-                                       label = "Sumbit Data"), width = 2),
+                                       label = "Submit Data"), width = 2),
               mainPanel(# actionButton("toggleSidebar", "Toggle sidebar"),
                         withSpinner(visNetworkOutput("network", height = "700px")),
-                        uiOutput("clust_select"),
                         withSpinner(dataTableOutput("enrich")),
                         uiOutput("downButton")
                     # tabPanel("Clusters",
@@ -57,7 +62,12 @@ ui <- fluidPage(
                     #          withSpinner(plotOutput("clusters", height = "900px")))
         )
       ),
-  inverse = T)
+  inverse = T)),
+  tabItem(tabName = "PCSF",
+          actionButton(inputId = "pcsf_submit", label = "Submit"),
+          withSpinner(visNetworkOutput("pcsf", height = "700px"))
+          )
+  ))
 )
 
 #generates output based on input
@@ -76,19 +86,19 @@ server <- function(input, output, session) {
   #program stores output of "reactive" functions so they don't have to reload when multiple elements call them
   string_db <- reactive({
     #creating stringdb object
-    isolate(score_thresh <- input$score_thresh)
+    isolate(score_thresh <- as.numeric(input$score_thresh))
     isolate(species <- as.numeric(input$species))
     STRINGdb$new(version="11.5", species=species, score_threshold=score_thresh, input_directory="/Users/lewi052/MAP/STRINGdb_exploration/STRINGdb_cache")
   })
   hits <- reactive({
     #reading file and mapping proteins (on button press)
-    if(input$submit >= 1) {
+    if (input$submit >= 1) {
     string_db <- string_db()
     
     #isolate makes it so element isn't reloaded when values change
     isolate(file <- input$in_file)
     isolate(included_prots <- input$included_prots)
-    isolate(Protein <- input$Protein)
+    isolate(Protein <- as.numeric(input$Protein))
     isolate(logFC <- input$logFC)
     isolate(P_val <- input$P_val)
     
@@ -114,11 +124,13 @@ server <- function(input, output, session) {
     mapped_stat <- mutate(mapped_stat, Fold_change_abs = abs(mapped_stat$Fold_change))
     mapped_stat <- mapped_stat[order(mapped_stat$`Fold_change_abs`, decreasing = T), ]
     
-    nodes <- mapped_stat[1: included_prots, c("Protein_Identifier", "STRING_id")]
-    colnames(nodes)  <- c("label", "id")
+    nodes <- mapped_stat[1: included_prots, c("Protein_Identifier", "STRING_id", "Gene_Name")]
+    colnames(nodes)  <- c("label", "id", "name")
     edges <- string_db$get_interactions(mapped_stat$STRING_id)
     edges <- edges[!duplicated(edges),]
-    colnames(edges) <- c("from", "to")
+    colnames(edges) <- c("from", "to", "width")
+    edges$width <- as.integer(as.numeric(edges$width)/100)
+    print(head(edges))
     list(nodes = nodes,
          edges = edges[,1:2])
   }
@@ -152,40 +164,44 @@ server <- function(input, output, session) {
     #creating network (on button press)
     if(input$submit >= 1) {
       nodes <- nodes()
+      colnames(nodes) <- c("x", "id", "label", "group")
+      # nodes <- nodes[!duplicated(nodes$label), ]
       hits <- hits()
       edges <- hits$edges
-      visNetwork(nodes = nodes, edges = edges) %>% visNodes(value = 45, font = list(size = 40), scaling = list(max = 75)) %>%
-        visEdges(width = 5) %>% visLayout(randomSeed = 123) %>% visIgraphLayout() %>% 
-        visInteraction(navigationButtons = T) %>% 
-        visOptions(selectedBy = list(variable = "group")) %>% visLegend(zoom = F, ncol = 2) %>%
-        visExport(type = "png", name = "exported-network", float = "right", 
+      # edges <- edges[nodes$id %in% edges$to & nodes$id %in% edges$from, ]
+      # graph <- graph_from_data_frame(d = edges, vertices = nodes, directed = F)
+      
+      # visIgraph(graph, physics = F)
+      visNetwork(nodes = nodes, edges = edges) %>% 
+        visNodes(value = 45, font = list(size = 40), scaling = list(max = 75), shadow = list(enabled = T, size = 10)) %>%
+        visEdges(width = 10) %>%
+        visInteraction(navigationButtons = T) %>% visIgraphLayout(randomSeed = 123) %>% visLegend(zoom = F) %>%
+        visEvents(select = "function(nodes) {
+                Shiny.onInputChange('sel_node', nodes.nodes);
+                ;}") %>%
+        visExport(type = "png", name = "exported-network", float = "right",
                   label = "Export PNG", background = "white", style= "")
     }
   })
-  #creates selection menu based on number of clusters found
-  observeEvent(input$submit, {
+
+  observeEvent(input$sel_node,{
     nodes <- nodes()
-    output$clust_select <- renderUI({
-      tagList(
-        selectInput("clusters", "Select a cluster", c("All Clusters", unique(nodes$group)))
-      )}
-    )
-  })
-  observeEvent(input$clusters, {
-    nodes = nodes()
-    if (input$clusters != "All Clusters") {
-      high_nodes <- filter(nodes, group == input$clusters)
-      visNetworkProxy("network") %>% visSelectNodes(high_nodes$id)
-    }
+    sel_node_row <- nodes[nodes$id == input$sel_node, ]
+    node_cluster <- filter(nodes, group == sel_node_row$group)
+    # node_exclude <- filter(nodes, group != sel_node_row$group)
+    visNetworkProxy("network") %>% visSelectNodes(node_cluster$id) %>% visFocus(id = input$sel_node, scale = 0.4)
   })
   
 #ENRICHMENT TABLE
   enrich_table <- reactive({
     string_db <- string_db()
     nodes <- nodes()
+    print(input$sel_node)
     #filters enrichment table based on selected cluster
-    if (input$clusters != "All Clusters")
-      nodes <- filter(nodes, group == input$clusters)
+    if (!is.null(input$sel_node)) {
+      sel_node_row <- nodes[nodes$id == input$sel_node, ]
+      nodes <- filter(nodes, group == sel_node_row$group)
+    }
     enrichment <- string_db$get_enrichment(nodes$id)
     enrichment
   })
@@ -207,36 +223,31 @@ server <- function(input, output, session) {
       write.csv(enrich_table(), file, row.names = F)
     }
   )
-  
-#CLUSTERS
-  # clusterList <- reactive({
-  #   string_db <- string_db()
-  #   hits <- hits()
-  #   string_db$get_clusters(hits$STRING_id)
-  # })
-  # output$clusters <-  renderPlot({
-  #   if (input$submit >= 1){
-  #   clusterList <- clusterList()
-  #   string_db <- string_db()
-  #   par(mfrow=c(2,2))
-  #   for(i in seq(1:4)){
-  #     string_db$plot_network(clusterList[[i]])
-  #   }
-  #   }
-  # })
-  # output$downloadClusters <- downloadHandler(
-  #   filename = "NetworkClusters.png",
-  #   content = function(file) {
-  #     png(file, width = 1400, height = 1100)
-  #     clusterList <- clusterList()
-  #     string_db <- string_db()
-  #     par(mfrow=c(2,2))
-  #     for(i in seq(1:4)){
-  #       string_db$plot_network(clusterList[[i]])
-  #     }
-  #     dev.off()
-  #   }
-  # )
+  output$pcsf <- renderVisNetwork({
+    if (input$pcsf_submit >= 1) {
+    
+    isolate(file <- input$in_file)
+    ext <- tools::file_ext(file$datapath)
+    if(ext == "csv"){
+      user_data <- read.csv(file$datapath)
+    } else {
+      user_data <- readRDS(file$datapath)
+    }
+    proStat_HvS <- user_data %>% filter(`Flag_A_Healthy Control_vs_Severe` != 0)
+    
+    geneList_HvS <- abs(proStat_HvS[,10])
+    gene_sym <- UniProt.ws::select(org.Hs.eg.db, proStat_HvS[,17], "SYMBOL","UNIPROT")
+    gene_sym <- gene_sym[!duplicated(gene_sym[,1]),]
+    names(geneList_HvS) = as.character(gene_sym[,2])
+    geneList_HvS <- sort(geneList_HvS, decreasing = T)
+    terminals <- geneList_HvS[1:50]
+    ppi <- construct_interactome(STRING)
+    subnet <- PCSF(ppi, terminals, w = 2, b = 1, mu = 0.0005)
+    visIgraph(subnet) %>% visNodes(value = 45, font = list(size = 40), scaling = list(max = 75), shadow = list(enabled = T, size = 10)) %>%
+      visInteraction(navigationButtons = T) %>%
+      visExport(type = "png", name = "exported-network", float = "right",
+                                    label = "Export PNG", background = "white", style= "")
+  }})
   
   #stops the app when browser window is closed
   session$onSessionEnded(stopApp)
