679cd3215f0b28b372ff278514cda061391a3381
Loglew12 <loganlewis51@gmail.com>
2022-10-12 14:01:06 -0700

    added env variable to distigush between docker and local

--- Prototype_app/server.R
+++ Prototype_app/server.R
@@ -1,7 +1,10 @@
 source("global.R")
 
 server <- function(input, output, session) {
-
+  #set to 0 for running local, 1 for running in docker
+  # Sys.setenv("DEMO_VERSION" = "0")
+  Sys.setenv("DEMO_VERSION" = "1")
+  
   # Reading in user uploaded file
   readUserData <- reactive({
     file <- input$inFile
@@ -14,6 +17,7 @@ server <- function(input, output, session) {
     userData
   })
   
+  #reading in the appropriate database files based on species
   mapping_db <- reactive({
     isolate(species <- input$species)
     make_mapping_db(species)
@@ -28,25 +32,36 @@ server <- function(input, output, session) {
     isolate(species <- input$species)
     make_enrich_db(species)
   })
+  
+  # Displays datatable of uploaded file
+  observeEvent(input$inFile, {
+    output$previewTable <- renderDataTable({
+      userData <- readUserData()
+      DT::datatable(userData, options = list(scrollX = TRUE))
+    }, options = list(pageLength = 10))})
 
+  #filtering user data
   filtUserData <- reactive({
     isolate(userData <- readUserData())
     isolate(Protein <- input$Protein)
     isolate(logFC <- input$logFC)
     isolate(P_val <- input$P_val)
+    isolate(P_val_cut <- input$P_val_cut)
     isolate(mapping_db <- mapping_db())
 
     #converting given column names to known ones
     colnames(userData)[which(names(userData) == Protein)] <- "Protein_Identifier"
     colnames(userData)[which(names(userData) == logFC)] <- "Fold_change"
     colnames(userData)[which(names(userData) == P_val)] <- "P_value"
+    
+    #filtering by provided p-value
+    filtUserData <- filter(userData, P_value <= input$P_val_cut)
 
-    filtUserData <- filter(userData, P_value <= 0.05)
-
-    #Adding abolute value of log fold change column
+    #Adding absolute value of log fold change column
     filtUserData <- mutate(filtUserData, Fold_change_abs = abs(filtUserData$Fold_change))
     filtUserData <- filtUserData[order(filtUserData$`Fold_change_abs`, decreasing = T), ]
     
+    #adding a STRING_id column (to find protein interactions) and a genename column (to find enrichment)
     filtUserData <- map_to_stringids(filtUserData, mapping_db)
     filtUserData$GeneName <- convert_to_genename(filtUserData$STRING_id, mapping_db)
 
@@ -69,26 +84,27 @@ server <- function(input, output, session) {
       print("getting interactions")
       edges <- get_interactions(hits, scoreThresh, inters_db)
       
+      #Currently proteins with no interations are not included in the final graph
+      #here I could manually add them back by checking the uploaded proteins vs the ones in edges now?
+      
+      #converting STRING_ids to genenames
       edges$names1 <- convert_to_genename(edges$protein1, mapping_db)
       edges$names2 <- convert_to_genename(edges$protein2, mapping_db)
       
+      #label columns appropriately
       colnames(edges) <- c("protein1", "protein2", "width", "from", "to")
       edges <- edges[,c("from", "to", "width", "protein1", "protein2")]
       edges$width <- as.numeric(edges$width)/100
-
+      
+      #convert to igraph object
       graph <- graph_from_data_frame(edges, directed = FALSE)
+      
+      #cluster with edge betweenness algorithm
       cluster <- cluster_edge_betweenness(graph)
       V(graph)$group <- cluster$membership
       return(graph)
     }
   })
-
-  # Reads in User uploaded file
-  observeEvent(input$inFile, {
-  output$previewTable <- renderDataTable({
-    userData <- readUserData()
-    DT::datatable(userData, options = list(scrollX = TRUE))
-    }, options = list(pageLength = 10))})
 
 #STRING NETWORK
 
@@ -123,6 +139,7 @@ server <- function(input, output, session) {
     isolate(filtUserData <- filtUserData())
     isolate(enrich_db <- enrich_db())
     graph <- graph()
+    #selects backgroud for enrichment based on user selection
     if(input$bgChoice == "full"){
       background <- filtUserData$GeneName
     }
@@ -133,7 +150,6 @@ server <- function(input, output, session) {
     #filters enrichment table based on selected cluster
     if (!is.null(input$sel_node)) {
       stringSelGroup <- V(graph)$group[V(graph)$name == input$sel_node]
-      # clust_enrich = find_enrich(V(graph)$name, V(graph)$name[which(V(graph)$group == stringSelGroup)], species, enrich_db)
       clust_enrich = find_enrich(background, V(graph)$name[which(V(graph)$group == stringSelGroup)], enrich_db)
     }
     else {
@@ -142,6 +158,7 @@ server <- function(input, output, session) {
     clust_enrich
   })
   
+  #edits table for display clarity
   edited_table <- reactive({
     if(input$submit >= 1) {
       enrich_table <- enrich_table()
@@ -149,25 +166,25 @@ server <- function(input, output, session) {
       edited_table
     }
   })
-  
+  #displays enrichment table
   output$enrich <- renderDataTable({
     edited_table <- edited_table()
   }, options = list(pageLength = 10), rownames = F)
   
+  #creates download buttons
   observeEvent(input$submit, {
     #creating enrichment table download button
     output$downButtonFull <- renderUI({downloadButton("downloadFull", "Download Full Enrichment")})
     output$downButtonCurrent <- renderUI({downloadButton("downloadCurrent", "Download Current Enrichment")})
   })
   
-  #operates Download button
+  #downloads edited or full table based on button click
   output$downloadFull <- downloadHandler(
     filename = "EnrichmentTableFull.csv",
     content = function(file) {
       write.csv(enrich_table(), file, row.names = F)
     }
   )
-  
   output$downloadCurrent <- downloadHandler(
     filename = "EnrichmentTableSection.csv",
     content = function(file) {
@@ -178,6 +195,7 @@ server <- function(input, output, session) {
 
 #PCSF NETWORK
   subnet <- reactive({
+    #checks for button click
     if (input$pcsfSubmit >= 1) {
 
     isolate(filtUserData <- filtUserData())
@@ -231,6 +249,7 @@ server <- function(input, output, session) {
     isolate(filtUserData <- filtUserData())
     isolate(enrich_db <- enrich_db())
     subnet <- subnet()
+    #making selection based on button click
     if(input$bgChoicePCSF == "full"){
       background <- unique(c(filtUserData$GeneName, V(subnet)$name))
     }
@@ -240,7 +259,6 @@ server <- function(input, output, session) {
     #filters enrichment table based on selected cluster
     if (!is.null(input$pcsf_node)) {
       pcsfSelGroup <- V(subnet)$group[V(subnet)$name == input$pcsf_node]
-      # clust_enrich = find_enrich(V(subnet)$name, V(subnet)$name[which(V(subnet)$group == pcsfSelGroup)], species)
       clust_enrich = find_enrich(background, V(subnet)$name[which(V(subnet)$group == pcsfSelGroup)], enrich_db)
     }
     else {
@@ -249,6 +267,7 @@ server <- function(input, output, session) {
     clust_enrich
   })
 
+  #edits table for display clarity
   edited_table_pcsf <- reactive({
     if(input$pcsfSubmit >= 1) {
       enrich_table <- enrich_table_pcsf()
@@ -257,6 +276,7 @@ server <- function(input, output, session) {
     }
   })
   
+  #displays table
   output$pcsf_enrich <- renderDataTable({
     edited_table_pcsf <- edited_table_pcsf()
   }, options = list(pageLength = 10), rownames = F)
@@ -267,24 +287,26 @@ server <- function(input, output, session) {
     output$downPCSFButtonCurrent <- renderUI({downloadButton("downloadPCSFCurrent", "Download Current Table")})
   })
 
-  #operates Download button
+  #operates full or current table Download buttons
   output$downloadPCSFFull <- downloadHandler(
     filename = "PCSFEnrichmentTableFull.csv",
     content = function(file) {
       write.csv(enrich_table_pcsf(), file, row.names = F)
     }
   )
-  
   output$downloadPCSFCurrent <- downloadHandler(
     filename = "PCSFEnrichmentTableSection.csv",
     content = function(file) {
       write.csv(edited_table_pcsf(), file, row.names = F)
     }
   )
-  output$complete <- renderVisNetwork({
+
+#SPRAS TESTING
+  #this section is for trying to test running spras from the R shiny app
+  output$sprasNet <- renderVisNetwork({
     if(input$sprasSubmit >= 1) {
       #loading in needed inputs
-      isolate(filtUserData <- filtUserData())
+      isolate(proData <- readUserData())
       isolate(species <- input$species)
       isolate(inters_db <- inters_db())
       isolate(mapping_db <- mapping_db())
@@ -292,27 +314,60 @@ server <- function(input, output, session) {
       isolate(scoreThresh <- as.numeric(input$scoreThresh))
       
       #grabbing the top absolute values log-fold change proteins, grabbing needed columns
-      hits <- filtUserData[1:includedProts, c("STRING_id", "Fold_change_abs")]
+      # hits <- filtUserData[, c("STRING_id", "Fold_change_abs")]
+      proData <- map_to_stringids(proData, mapping_db)
       
       #Getting interactions from the string_db that contain 'hits' proteins
-      edges <- get_interactions(hits$STRING_id, scoreThresh, inters_db)
-      colnames(edges) <- c("protein1", "protein2", "cost")
-      edges$cost <- as.character((1000 - edges$cost)/1000)
+      # edges <- get_interactions(proData$STRING_id, scoreThresh, inters_db)
+      colnames(inters_db) <- c("protein1", "protein2", "cost")
+      inters_db$cost <- as.character((1000 - inters_db$cost)/1000)
+      print(head(inters_db))
       
       #changing to appropriate column names and adding (arbitrary) sources and targets columns
-      colnames(hits) <- c("NODEID", "prize")
-      hits$sources <- rep("True", times = nrow(hits))
-      hits$targets <- rep("True", times = nrow(hits))
+      # colnames(hits) <- c("NODEID", "prize")
+      # hits$sources <- rep("True", times = nrow(hits))
+      # hits$targets <- rep("True", times = nrow(hits))
+      outFile <- proData[,c('STRING_id','Fold_change_Healthy Control_vs_Severe')]
+      colnames(outFile) <- c('NODEID', 'prize')
+      outFile$sources <- rep("True", times = nrow(outFile))
+      outFile$targets <- rep("True", times = nrow(outFile))
+      outFile$prize <- as.character(abs(outFile$prize))
+      
+      print(head(outFile))
       
       #saving nodes and edges to files
-      write_tsv(hits, "/Users/lewi052/enrich_proj/spras/input2/shinyPrizes.tsv")
-      write_tsv(edges, "/Users/lewi052/enrich_proj/spras/input2/shinyEdges.tsv", col_names = F)
+      if(Sys.getenv("DEMO_VERSION") == "0"){
+      write_tsv(outFile, "/Users/lewi052/enrich_proj/MAP/Prototype_app/spras/input2/shinyPrizes.tsv")
+      write_tsv(inters_db, "/Users/lewi052/enrich_proj/MAP/Prototype_app/spras/input2/shinyEdges.tsv", col_names = F)
+      }
+      else if(Sys.getenv("DEMO_VERSION") == "1"){
+      write_tsv(outFile, "/app/spras/input2/shinyPrizes.tsv")
+      write_tsv(inters_db, "/app/spras/input2/shinyEdges.tsv", col_names = F)
+      }
       
       #running the snakemake command
-      system("snakemake --cores 1 --configfile config/config_copy.yaml")
+      # system("cd spras")
+      if(Sys.getenv("DEMO_VERSION") == "0"){
+        setwd("/Users/lewi052/enrich_proj/MAP/Prototype_app/spras")
+        system("snakemake --cores 1 --configfile config/config_copy.yaml")
+      }
+      else if(Sys.getenv("DEMO_VERSION") == "1"){
+      setwd("/app/spras")
+      system("export https_proxy=\"http://proxy01.pnl.gov:3128\"")
+      system("export HTTPS_PROXY=\"http://proxy01.pnl.gov:3128\"")
+      system("snakemake --cores 1 --configfile /app/spras/config/config_copy.yaml")
+      # system("docker run reedcompbio/omics-integrator-2 OmicsIntegrator -e /app/spras/input2/shinyEdges.tsv -p /app/spras/input2/shinyEdges.tsv -o /app/spras/output2")
+      }
       
       #reading in the snakemake output and converting to igraph graph object
-      outData <- read.table("/Users/lewi052/enrich_proj/spras/output2/data0-omicsintegrator2-params-IV3IPCJ/pathway.txt")
+      if(Sys.getenv("DEMO_VERSION") == "0"){
+      # outData <- read.table("/Users/lewi052/enrich_proj/MAP/Prototype_app/spras/output2/data0-omicsintegrator2-params-EHHWPMD/pathway.txt")
+      }
+      else if(Sys.getenv("DEMO_VERSION") == "1"){
+      #outData <- read.table("/app/spras/output/data0-omicsintegrator2-params-EHHWPMD/pathway.txt")
+      outData <- read.table("/app/spras/output2/data0-omicsintegrator2-params-IV3IPCJ/pathway.txt")
+      # outData <- read.csv("/out_data/testDataGraph/graph.tsv", sep = "\t")
+      }
       graph <- graph_from_data_frame(outData)
       
       #clustering graph and changing node names to readable gene names
